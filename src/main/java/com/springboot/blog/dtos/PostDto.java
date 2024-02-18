e4b1bcc29b87f5a4ccac818832550ebd7627cba1
nguetcheu <dnguetcheu@gmail.com>
2024-02-18 09:13:27 +0100

    Création du package du DTO et de la classe DTO pour l'entité Post

---
+++ src/main/java/com/springboot/blog/dtos/PostDto.java
@@ -0,0 +1,13 @@
+package com.springboot.blog.dtos;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private Long id;
+
+    private String title;
+    private String description;
+    private String content;
+
+}
