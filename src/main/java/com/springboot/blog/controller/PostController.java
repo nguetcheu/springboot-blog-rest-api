8970fb0264a6e47f719eb28472e1386d0750b4a3
nguetcheu <dnguetcheu@gmail.com>
2024-02-22 09:56:16 +0100

    Refactorisation du code et usage de constant pour les valeurs par défaut des paramétres de recherche

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -3,6 +3,7 @@ package com.springboot.blog.controller;
 import com.springboot.blog.dtos.PostDto;
 import com.springboot.blog.dtos.PostResponse;
 import com.springboot.blog.service.PostService;
+import com.springboot.blog.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,10 +23,10 @@ public class PostController {
     @GetMapping
     public PostResponse getAllPosts(
             // paramétre de trie avec api obtention de post http://localhost:9000/api/posts?sortBy=title&pageSize=10&pageNo?=0&sortDir=asc
-            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
 
     ){
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
