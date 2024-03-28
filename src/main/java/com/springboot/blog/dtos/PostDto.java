017a6d6f12acbe98fa06b030796be1b5f53b4085
nguetcheu <dnguetcheu@gmail.com>
2024-03-28 11:14:58 +0100

    fix du bug de l'affichage des commentaires sur chaque post avec @Getter et @Setter

--- src/main/java/com/springboot/blog/dtos/PostDto.java
+++ src/main/java/com/springboot/blog/dtos/PostDto.java
@@ -2,11 +2,14 @@ package com.springboot.blog.dtos;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDto {
     private Long id;
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 
 }
