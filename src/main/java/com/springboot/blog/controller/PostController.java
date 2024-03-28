001e8e59f8dc674098aecbe33a2b3e9c8653bf34
nguetcheu <dnguetcheu@gmail.com>
2024-03-28 12:30:03 +0100

    Gestion de la validation des post lors de l'insertion

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -4,6 +4,7 @@ import com.springboot.blog.dtos.PostDto;
 import com.springboot.blog.dtos.PostResponse;
 import com.springboot.blog.service.PostService;
 import com.springboot.blog.utils.AppConstants;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,13 +33,19 @@ public class PostController {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
+    // create the post
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
+        return new ResponseEntity<PostDto>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<PostDto> getPostsById(@PathVariable("id") Long postId){
         return ResponseEntity.ok(postService.getPostById(postId));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatePostById(@RequestBody PostDto postDto, @PathVariable("id") Long postId){
+    public ResponseEntity<PostDto> updatePostById(@Valid @RequestBody PostDto postDto, @PathVariable("id") Long postId){
         PostDto postResponse = postService.updatePost(postDto, postId);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
@@ -49,11 +56,5 @@ public class PostController {
         postService.deletePostById(postId);
 
         return new ResponseEntity<>("Post entity deleted successfully",HttpStatus.OK);
-    }
-
-    // create the post
-    @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
-        return new ResponseEntity<PostDto>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 }
