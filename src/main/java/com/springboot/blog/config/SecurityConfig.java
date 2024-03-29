8eee75dc1cb9aa1402050031fcfe98da9a0eb999
nguetcheu <dnguetcheu@gmail.com>
2024-03-29 20:01:48 +0100

    Ajout de l'annotation de @configuration et gestion du securityFilterChain

---
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.springboot.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+// ajout de l'annotation de @configuration et gestion du securityFilterChain
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf()
+                .disable()
+                .authorizeHttpRequests( (authorize) ->  authorize.anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
