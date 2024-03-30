239120077e0fcfa25c70d4d839f8faa2a200618a
nguetcheu <dnguetcheu@gmail.com>
2024-03-30 09:16:25 +0100

    Ajout d'une méthode permettant de créer les utilisateurs et les attribuer un role sur nos API en les stockant en mémoire

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -4,11 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 // ajout de l'annotation de @configuration et gestion du securityFilterChain
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,5 +30,23 @@ public class SecurityConfig {
                 ).httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    // méthode permettant de créer les utilisateurs et les attribuer un role sur nos API
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails nguetcheu = User.builder()
+                .username("nguetcheu")
+                .password(passwordEncoder().encode("nguetcheu"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(nguetcheu, admin);
     }
 }
