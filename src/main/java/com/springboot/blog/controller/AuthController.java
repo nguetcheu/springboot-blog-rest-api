f08a31bb3fa38ea19c86ee72e5e2f5c1a1238b2f
nguetcheu <dnguetcheu@gmail.com>
2024-04-01 11:01:56 +0200

    Ajout de l'accés a la méthode login par tout les utilisateurs  étant authentifié

--- src/main/java/com/springboot/blog/controller/AuthController.java
+++ src/main/java/com/springboot/blog/controller/AuthController.java
@@ -1,4 +1,27 @@
 package com.springboot.blog.controller;
 
+import com.springboot.blog.dtos.LoginDto;
+import com.springboot.blog.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    // Build login REST API
+    @PostMapping(value = {"/login", "/signin"})
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+        String response = authService.login(loginDto);
+        return ResponseEntity.ok(response);
+    }
 }
