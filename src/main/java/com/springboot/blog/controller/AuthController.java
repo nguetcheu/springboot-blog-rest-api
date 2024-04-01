24f62e9018e7bbee6e5d9c945da98ae54634d2f8
nguetcheu <dnguetcheu@gmail.com>
2024-04-01 11:21:50 +0200

    Création de la méthode du endpoint d'inscription dans le controlleur authentification

--- src/main/java/com/springboot/blog/controller/AuthController.java
+++ src/main/java/com/springboot/blog/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.dtos.LoginDto;
+import com.springboot.blog.dtos.RegisterDto;
 import com.springboot.blog.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,5 +25,12 @@ public class AuthController {
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
         String response = authService.login(loginDto);
         return ResponseEntity.ok(response);
+    }
+
+    // Build Register REST API
+    @PostMapping(value={"/register","/signup"})
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
