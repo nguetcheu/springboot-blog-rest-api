7f03aac6feb30cb3dae0d8feceda31b03c985f51
nguetcheu <dnguetcheu@gmail.com>
2024-04-19 00:17:51 +0200

    Création du repository et du dto ok

--- src/main/java/com/springboot/blog/repository/CategoryRepository.java
+++ src/main/java/com/springboot/blog/repository/CategoryRepository.java
@@ -1,4 +1,7 @@
 package com.springboot.blog.repository;
 
-public class CategoryRepository {
+import com.springboot.blog.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
